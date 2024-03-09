5aa5a6acb2bea01a4d511579626f7ac11cf0ae89
Leon Brocard <acme@astray.com>
2024-03-09 14:49:21 +0000

    Try line

--- teacup.py
+++ teacup.py
@@ -1,7 +1,7 @@
 from build123d import *
 from ocp_vscode import show
 
-length, width, thickness = 80.0, 60.0, 10.0
+# length, width, thickness = 80.0, 60.0, 10.0
 
 with BuildPart() as ex:
     with Locations((-25, 0, 0), (25, 0, 0)):
@@ -12,6 +12,19 @@ with BuildPart() as ex:
     Cylinder(10, 50, rotation=(0, 90, 0))
     with Locations((0, 0, 25/4)):
         Box(100, 100, 25/2, mode=Mode.INTERSECT)
+
+# with BuildPart() as ex:
+#     with BuildLine() as ex_ln:
+#         l3 = Line((0, -25), (25, 25))
+#     with BuildSketch(Plane.XZ) as ex_sk:
+#         Circle(10)
+#     sweep()
+#     with Locations((0, -25, 0), (25, 25, 0)):
+#         Sphere(radius=10)
+
 show(ex)
 
-ex.part.export_step("teacup.step")
+
+
+# ex.part.export_step("teacup.step")
+# ex.part.export_step("teacup.stl")
