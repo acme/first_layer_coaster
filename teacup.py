d82328ba47908b16bc87cd715b91b79498b4158b
Leon Brocard <acme@astray.com>
2024-03-09 15:11:08 +0000

    It works!

--- teacup.py
+++ teacup.py
@@ -3,24 +3,27 @@ from ocp_vscode import show
 
 # length, width, thickness = 80.0, 60.0, 10.0
 
+# with BuildPart() as ex:
+#     with Locations((-25, 0, 0), (25, 0, 0)):
+#         Sphere(radius=10)
+#     Cylinder(10, 50, rotation=(90, 0, 0))
+#     with Locations((0, -25, 0), (0, 25, 0)):
+#         Sphere(radius=10)
+#     Cylinder(10, 50, rotation=(0, 90, 0))
+#     split(bisect_by=Plane.XY)
+
+x1, y1, x2, y2 = 0, 0, 25, 25
+
 with BuildPart() as ex:
-    with Locations((-25, 0, 0), (25, 0, 0)):
+    with BuildLine() as ex_ln:
+        l1 = Line((x1, y1), (x2, y2))
+    with BuildSketch(Plane(origin=l1 @ 0, z_dir=l1 % 0)) as ex_sk:
+        Circle(10)
+    sweep()
+    with Locations((x1, y1, 0), (x2, y2, 0)):
         Sphere(radius=10)
-    Cylinder(10, 50, rotation=(90, 0, 0))
-    with Locations((0, -25, 0), (0, 25, 0)):
-        Sphere(radius=10)
-    Cylinder(10, 50, rotation=(0, 90, 0))
     split(bisect_by=Plane.XY)
 
-# with BuildPart() as ex:
-#     with BuildLine() as ex_ln:
-#         l3 = Line((0, -25), (25, 25))
-#     with BuildSketch(Plane.XZ) as ex_sk:
-#         Circle(10)
-#     sweep()
-#     with Locations((0, -25, 0), (25, 25, 0)):
-#         Sphere(radius=10)
-
 show(ex)
-ex.part.export_step("teacup.step")
-ex.part.export_step("teacup.stl")
+# ex.part.export_step("teacup.step")
+# ex.part.export_step("teacup.stl")
